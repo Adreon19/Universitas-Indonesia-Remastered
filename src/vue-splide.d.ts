34b1db734049fbeaeeef3fe2e5b80dd1a3244b25
Adreon <sachioramzy12@gmail.com>
2024-10-14 17:11:23 +0700

    declaring vue-splide module

---
+++ src/vue-splide.d.ts
@@ -0,0 +1,7 @@
+// src/types/vue-splide.d.ts
+declare module "@splidejs/vue-splide" {
+  import { DefineComponent } from "vue";
+
+  export const Splide: DefineComponent;
+  export const SplideSlide: DefineComponent;
+}
