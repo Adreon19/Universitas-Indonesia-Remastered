d81cac80080301f6250806e5faccdbc807a9d744
Adreon <sachioramzy12@gmail.com>
2024-10-17 10:18:12 +0700

    add some padding

--- src/components/Home/Slideshow.vue
+++ src/components/Home/Slideshow.vue
@@ -1,23 +1,29 @@
-<template>
-  <Splide :options="{ type: 'loop', autoplay: true }">
-    <SplideSlide>
-      <img src="/public/slide/Profesi.jpg" alt="Slide 1" />
-    </SplideSlide>
-    <SplideSlide>
-      <img src="/public/slide/S2.jpg" alt="Slide 2" />
-    </SplideSlide>
-    <SplideSlide>
-      <img src="/public/slide/S3.jpg" alt="Slide 3" />
-    </SplideSlide>
-    <SplideSlide>
-      <img src="/public/slide/Spesialis.jpg" alt="Slide 3" />
-    </SplideSlide>
-  </Splide>
-</template>
-
 <script setup>
-const Splide = (await import("@splidejs/vue-splide")).Splide;
-const SplideSlide = (await import("@splidejs/vue-splide")).SplideSlide;
-
+import { Splide, SplideSlide } from "@splidejs/vue-splide";
 import "@splidejs/splide/dist/css/splide.min.css";
 </script>
+
+<template>
+  <div class="slideshow">
+    <Splide :options="{ type: 'loop', autoplay: true }">
+      <SplideSlide>
+        <img src="/slide/Profesi.jpg" alt="Slide 1" />
+      </SplideSlide>
+      <SplideSlide>
+        <img src="/slide/S2.jpg" alt="Slide 2" />
+      </SplideSlide>
+      <SplideSlide>
+        <img src="/slide/S3.jpg" alt="Slide 3" />
+      </SplideSlide>
+      <SplideSlide>
+        <img src="/slide/Spesialis.jpg" alt="Slide 3" />
+      </SplideSlide>
+    </Splide>
+  </div>
+</template>
+<style scoped>
+.slideshow {
+  padding: 50px;
+  padding-top: 0;
+}
+</style>
