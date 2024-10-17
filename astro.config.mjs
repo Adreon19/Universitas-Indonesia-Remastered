4a0bfc7ed7456f286ef2cd09af08f480ce5fb6cb
Adreon <sachioramzy12@gmail.com>
2024-10-17 17:09:06 +0700

    adding iconify, astro-icons,

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,12 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import vue from "@astrojs/vue";
+import icons from "astro-icon";
+import mdi from "@iconify-json/mdi";
+import { collections } from "./src/content/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "http://localhost",
-  integrations: [mdx(), sitemap(), vue()],
+  integrations: [mdx(), sitemap(), vue(), icons({ collections: { mdi } })],
 });
