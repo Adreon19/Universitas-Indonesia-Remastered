04b215f24c63335c4714c9608426ccc78976831e
Adreon <sachioramzy12@gmail.com>
2024-10-18 15:30:35 +0700

    changing imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,8 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import vue from "@astrojs/vue";
 import icons from "astro-icon";
-import mdi from "@iconify-json/mdi";
-import { collections } from "./src/content/config";
+import * as mdi from "@iconify-json/mdi";
 
-// https://astro.build/config
 export default defineConfig({
   site: "http://localhost",
   integrations: [mdx(), sitemap(), vue(), icons({ collections: { mdi } })],
